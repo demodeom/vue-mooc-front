63bb9a3d27a9d18924badf9e204d2fa5d3ab1763
wyj <wyj@pop-os.localdomain>
2020-08-28 14:35:09 +0800

    save

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import axios from 'axios'
+import VueAxios from "vue-axios"
+
+import iView from 'iview'
+
+import 'iview/dist/styles/iview.css'
+
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = 'http://api.mooc.cc'
+
+Vue.use(VueAxios, axios)
+Vue.use(iView)
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
